9e768a6baf4153674aa1547cdf6ec74f2e7d1fb5
Ben Lacy <benlacy5@gmail.com>
2024-03-05 22:46:07 -0600

    formatting

--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -31,6 +31,7 @@ return {
 				"lua_ls",
 				"rust_analyzer",
 				"tsserver",
+				"tailwindcss",
 			},
 			handlers = {
 				function(server_name) -- default handler (optional)
--- lua/config/plugins/telescope.lua
+++ lua/config/plugins/telescope.lua
@@ -1,50 +1,50 @@
 return {
-    "nvim-telescope/telescope.nvim",
+	"nvim-telescope/telescope.nvim",
 
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        {
-        	'nvim-telescope/telescope-fzf-native.nvim',
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		{
+			"nvim-telescope/telescope-fzf-native.nvim",
 
 			-- `build` is used to run some command when the plugin is installed/updated.
 			-- This is only run then, not every time Neovim starts up.
-			build = 'make',
+			build = "make",
 
 			-- `cond` is a condition used to determine whether this plugin should be
 			-- installed and loaded.
 			cond = function()
-			  return vim.fn.executable 'make' == 1
+				return vim.fn.executable("make") == 1
 			end,
-      },
-      'nvim-telescope/telescope-ui-select.nvim',
+		},
+		"nvim-telescope/telescope-ui-select.nvim",
 
-      'nvim-tree/nvim-web-devicons',
+		"nvim-tree/nvim-web-devicons",
 	},
 
-    config = function()
-        require('telescope').setup({
+	config = function()
+		require("telescope").setup({
 			extensions = {
-			  ['ui-select'] = {
-				require('telescope.themes').get_dropdown(),
-			  },
+				["ui-select"] = {
+					require("telescope.themes").get_dropdown(),
+				},
 			},
 		})
-		pcall(require('telescope').load_extension, 'fzf')
-		pcall(require('telescope').load_extension, 'ui-select')
+		pcall(require("telescope").load_extension, "fzf")
+		pcall(require("telescope").load_extension, "ui-select")
 
-        local builtin = require('telescope.builtin')
+		local builtin = require("telescope.builtin")
 
-        -- vim.keymap.set('n', '<leader>f', builtin.git_files, {})
-        vim.keymap.set('n', '<leader>f', builtin.find_files, {desc = "[S]earch Files"})
-		vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
-		vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-		vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-		vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-		vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-		vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-		vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-		vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
-		vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-		vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' }) 
+		-- vim.keymap.set('n', '<leader>f', builtin.git_files, {})
+		vim.keymap.set("n", "<leader>f", builtin.find_files, { desc = "[S]earch Files" })
+		vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
+		vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
+		vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
+		vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
+		vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
+		vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
+		vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
+		vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
+		vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+		vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
 	end,
 }
